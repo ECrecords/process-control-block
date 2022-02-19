9a2f0fcaef52833df3c6a4365a27ad2e8e15dbba
ECrecords <elvis.chinoislas.271@my.csun.edu>
2022-02-18 22:56:08 -0800

    ECrecords: created menu

--- proc_cntrl_hier.c
+++ proc_cntrl_hier.c
@@ -21,14 +21,45 @@ typedef struct parent_proc parent_proc;
 
 int main(int argc, char const *argv[])
 {
-    int exit_flag = 1;
-    
+    short option;
     parent_proc pcb_arr[5];
     
-    while (exit_flag)
+    printf( "=================================================\n"
+            "Process Creation Hierarchy\n"
+            "\t1) Enter parameters\n"
+            "\t2) Create a new child process\n"
+            "\t3) Destroy all descendants of a process\n"
+            "\t4) Quit program and free memory\n"
+            "=================================================");
+
+    while (1)
     {
+        printf("\nEnter selection: ");
+        scanf("%d", &option);
+        
+        if (option == 1)
+        {
+            /* code */
+        } else if (option == 2)
+        {
+            /* code */
+        } else if (option == 3)
+        {
+            /* code */
+        } else if (option == 4)
+        {
+            break;
+        } else
+        {
+            printf("INVALID OPTION");
+        }
+        
+        
+        
+        
         
     }
     
     return 0;
 }
+
